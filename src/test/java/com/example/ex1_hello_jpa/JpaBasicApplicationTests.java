4251aa152a23ab9c13405d7a4639703e6a370125
zeongii <yaksanjy@naver.com>
2025-03-20 22:17:54 +0900

    H2 데이터베이스 연결

---
+++ src/test/java/com/example/ex1_hello_jpa/JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex1_hello_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
